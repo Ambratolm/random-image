280c29db140b1d645edf73fc9790481ea2013f5f
Ambratolm <Ambratolm@gmail.com>
2022-02-10 21:31:56 +0000

    Updated Image Sending Logic

--- index.php
+++ index.php
@@ -1,22 +1,28 @@
 <?php
-header('Content-Type: image/jpg');
-
+// Get All Posts in Feed
 $feed_url = "https://feeds.feedburner.com/ambratolm-cf";
 $feed_xml = file_get_contents($feed_url);
 $feed = new SimpleXMLElement($feed_xml);
-
 $entries = $feed->entry;
+
+// Get Random Post from Posts in Feed
 $entry_index = rand(0, count($entries) - 1);
 $entry = $entries[$entry_index];
 
+// Get All Images in Post
 $content = new DOMDocument();
 $content->loadHTML($entry->content);
-
 $imgs = $content->getElementsByTagName("img");
+
+// Get Random Image from Images in Post
 $img_index = rand(0, count($imgs) - 1);
 $img = $imgs[$img_index];
 $src = $img->getAttribute("src");
 
-$image = file_get_contents($src);
-echo $image;
+// Send Image to Browser
+$headers = get_headers($src, true);
+header("Content-Type: " . $headers["Content-Type"]);
+header("Content-Length: " . $headers["Content-Length"]);
+readfile($src);
+exit;
 ?>
